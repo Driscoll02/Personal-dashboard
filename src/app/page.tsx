bb8a5b2d12ba391ac00168bd02390c4fa5740ae2
Joseph Driscoll <driscoll2507@gmail.com>
2023-12-10 13:49:53 +0000

    Set up dashboard for widget creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="h-screen w-screen bg-gradient-to-r from-gray-900 to-gray-800 text-gray-200 ">
+      <h1 className="p-5 pl-10 text-5xl">Dashboard</h1>
     </div>
   );
 }
